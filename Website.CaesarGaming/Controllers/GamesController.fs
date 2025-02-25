b0ae7f1195a7c8a34010b7aa8daa0efc5de11146
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2025-02-25 23:09:27 +0100

    Commit _Layout

--- Website.CaesarGaming/Controllers/GamesController.fs
+++ Website.CaesarGaming/Controllers/GamesController.fs
@@ -1,29 +1,27 @@
 ﻿namespace Website.CaesarGaming.Controllers
 
 open System
-open System.Collections.Generic
-open System.Linq
-open System.Threading.Tasks
 open System.Diagnostics
-
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
-
 open Website.CaesarGaming.Models
 
-type GamesController (logger : ILogger<GamesController>) =
+[<Route("Games")>] // Route prefix for the controller
+type GamesController(logger: ILogger<GamesController>) =
     inherit Controller()
 
-    member this.Index () =
+
+    // Maps to /Games/Games
+    [<HttpGet("Games")>]
+    member this.Games() =
         this.View()
 
-
+    // Error handling method
     [<ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)>]
-    member this.Error () =
-        let reqId = 
-            if isNull Activity.Current then
-                this.HttpContext.TraceIdentifier
-            else
-                Activity.Current.Id
+    member this.Error() =
+        let reqId =
+            match Activity.Current with
+            | null -> this.HttpContext.TraceIdentifier
+            | current -> current.Id
 
         this.View({ RequestId = reqId })
